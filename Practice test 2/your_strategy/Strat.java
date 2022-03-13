25985c609c146e5cc8c59932b0ea53d46360f9eb
Danis Alukaev <aldanis73@yandex.ru>
2022-03-13 08:34:28 +0300

    + random best increase

--- Practice test 2/your_strategy/Strat.java
+++ Practice test 2/your_strategy/Strat.java
@@ -1,21 +1,33 @@
 package gametheory.assignment2.your_strategy;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import gametheory.assignment2.Player;
+import java.util.*;
 
 
 public class Strat implements Player {
 
+    private final ArrayList<Integer> moves;
+
+    public Strat() {
+        moves = new ArrayList<>();
+    }
+
     @Override
     public void reset() {
-        // kek
+        moves.clear();
     }
 
     @Override
     public int move(int opponentLastMove, int xA, int xB, int xC) {
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(xA, xB, xC));
-        int choice = 1 + numbers.indexOf(Collections.max(numbers));
+        final ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(xA, xB, xC));
+        final int maxElement = Collections.max(numbers);
+        final ArrayList<Integer> maxElements = new ArrayList<>(3);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i).equals(maxElement)) {
+                maxElements.add(i + 1);
+            }
+        }
+        int choice = maxElements.get(new Random().nextInt(maxElements.size()));
+        moves.add(choice);
         return choice;
     }
 
